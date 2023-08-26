d989bab07fe68003ed19eca4f960beeeaaaedb96
m0lts <moltontom@outlook.com>
2023-08-26 13:56:34 +0100

    press page complete

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import '../assets/global.css';
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Navbar() {
+
+    const [activeItem, setActiveItem] = useState('');
+
+    const handleItemClick = (itemName) => {
+        setActiveItem(itemName);
+    };
+
     return (
         <ul className="navbar">
             <li className="navItem">
-                <Link to='/' className="link">About</Link>
+                <Link to='/' className={`link ${activeItem === 'About' ? 'active' : ''}`} onClick={() => handleItemClick('About')}>About</Link>
             </li>
             <li className="navItem">
-                <Link to='/gallery' className="link">Gallery</Link>
+                <Link to='/gallery' className={`link ${activeItem === 'Gallery' ? 'active' : ''}`} onClick={() => handleItemClick('Gallery')}>Gallery</Link>
             </li>
             <li className="navItem">
-                <Link to='/press' className="link">Press</Link>
+                <Link to='/press' className={`link ${activeItem === 'Press' ? 'active' : ''}`} onClick={() => handleItemClick('Press')}>Press</Link>
             </li>
             <li className="navItem">
-                <Link to='/shop' className="link">Shop</Link>
+                <Link to='/shop' className={`link ${activeItem === 'Shop' ? 'active' : ''}`} onClick={() => handleItemClick('Shop')}>Shop</Link>
             </li>
             <li className="navItem">
-                <Link to='/contact' className="link">Contact</Link>
+                <Link to='/contact' className={`link ${activeItem === 'Contact' ? 'active' : ''}`} onClick={() => handleItemClick('Contact')}>Contact</Link>
             </li>
         </ul>
     )
--- src/pages/press.jsx
+++ src/pages/press.jsx
@@ -4,7 +4,25 @@ import '../assets/global.css';
 export default function Press() {
     return (
         <section className="press_page">
-            <h1>Press page</h1>
+            <h1>Press</h1>
+            <div className="press_container">
+                <div className="press_title_and_date">
+                    <h2 className="press_title">HS Projects</h2>
+                    <h3 className="press_date">August 2022</h3>
+                    <a target="blank" href="https://hsprojects.com/artist_name/tilly-flory/" className="press_link">https://hsprojects.com/artist_name/tilly-flory/</a>
+                </div>
+                <div className="press_text">
+                    <p>“Tilly Flory’s practice has guided her to a deeper understanding of the experience of grief. 
+                        Her painting Shrouded in Folds was made to represent the experience of life becoming internalised 
+                        while one processes grief, as well as a transition from the person they were before to the person 
+                        they become after encountering loss. Grief is similar to that of a caterpillar entering its chrysalis
+                         and emerging as something changed, perhaps more complex than it once was.”</p>
+                </div>
+                <div className="press_imgs_cont">
+                    <img src="../../public/images/press_imgs/shrouded_in_folds.jpeg" alt="" />
+                    <img src="../../public/images/press_imgs/shrouded_in_folds2.jpeg" alt="" />
+                </div>
+            </div>
         </section>
     )
 }
